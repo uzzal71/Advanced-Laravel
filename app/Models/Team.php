8e6239cb85ea2c28277ffc61818e8565eb05f729
uzzal71 <uzzalroy.acm@gmail.com>
2022-08-17 00:39:09 +0600

    Custom accessors

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Team extends Model
 {
     use HasFactory;
+
+    public function getUsersCountAttribute()
+    {
+    	return \DB::table('users')->where('users.team_id', $this->id)->sum('users.id');
+    }
+
+    public $appends = ['users_count'];
 }
