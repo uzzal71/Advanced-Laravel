176e3c351d0147eae39a7e6d16208244043ddae4
uzzal71 <uzzalroy.acm@gmail.com>
2022-08-17 00:49:18 +0600

    Advanced wheres

--- app/Teams/Repository.php
+++ app/Teams/Repository.php
@@ -5,9 +5,14 @@ class Repository
 {
 	public function points($team) 
 	{
+		$users = $team->where('teams.id', $team->id)
+			->join('users', 'teams.id', '=', 'users.team_id')
+			->select('users.id');
+
 		return $team->where('teams.id', $team->id)
             ->join('tickets', 'teams.id', '=', 'tickets.team_id')
             ->join('points', 'tickets.id', '=', 'points.ticket_id')
+            ->whereIn('points.owner_id', $users)
             ->sum('points.value');
 	}
 }
