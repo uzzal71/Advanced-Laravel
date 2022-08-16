aa1bf358559af2d5e07ea80a6482d1b31a6adfaf
uzzal71 <uzzalroy.acm@gmail.com>
2022-08-16 23:52:34 +0600

    Eloquent events

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function($model) {
+            $model->team_id = \DB::table('teams')->inRandomOrder()->first()->id;
+        });
+    }
 }
